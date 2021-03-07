d0b5a87feb5c51d042ea40b6068590d30df48718
陈阳 <1780552053@qq.com>
2021-03-07 17:21:49 +0800

    行程详情/列表完成

--- src/pages/addjourney/addjourney.vue
+++ src/pages/addjourney/addjourney.vue
@@ -1,10 +1,20 @@
 <template>
-  
+  <div>
+      <div>
+          0
+          <router-link to="journeyinfor">home</router-link>
+          <button @click="fn()">跳转</button>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+methods:{
+    fn(){
+        this.$router.push({name:'journeyinfor',params:{aaa:'111'}})
+    }
+}
 }
 </script>
 
--- src/pages/hotel/hotel.vue
+++ src/pages/hotel/hotel.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-      
+      <div>
+          头部
+      </div>
+      <div class="banner">
+          banner
+      </div>
+      <div class="content">
+          <div class="tit">
+              <span>蓝色酒店</span><span>1239.00</span>
+
+          </div>
+          <div class="star">
+              ⭐⭐⭐⭐⭐
+          </div>
+          <div class="infor">
+              一家精致的私人星级酒店，环境优雅。服务周到，让您有宾至如归的感觉
+
+          </div>
+          <div class="icon">
+              <div class="item">
+                  电话
+              </div>
+              <div class="item">
+                  邮件
+              </div>
+              <div class="item">
+                  地址
+              </div>
+              <div class="item">
+                  官网
+              </div>
+          </div>
+          <h3>酒店设施</h3>
+          <div class="sheshi">
+              <div class="item">
+                  免费wifi
+              </div>
+              <div class="item">
+                  中央空调
+              </div>
+          </div>
+          <div class="btn">
+              预定酒店
+          </div>
+      </div>
   </div>
 </template>
 
@@ -11,5 +55,37 @@ export default {
 </script>
 
 <style>
-
+.banner{
+    width: 100%;
+    height: 300px;
+}
+.icon{
+    display: flex;
+}
+.sheshi{
+    display: flex;
+}
+.content{
+    padding: 20px;
+}
+.tit{
+    padding: 10px 0;
+    font-size: 20px;
+}
+.item{
+    width: 50%;
+}
+.star{
+    padding: 20px 0;
+}
+.infor{
+    padding: 20px 0;
+}
+.btn{
+    margin-top: 20px;
+    background: skyblue;
+    text-align: center;
+    padding: 20px 0;
+    border-radius: 10px;
+}
 </style>
