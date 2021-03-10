88dd5444f4875fc7433e71643eff61ca06f84e61
谢广 <2330413171@qq.com>
2021-03-11 00:33:14 +0800

    my

--- src/pages/myjourney/didian.vue
+++ src/pages/myjourney/didian.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
       <ul class="box">
-          <li>
-              <img src="../../assets/images/my/bjt.png" alt="">
+          <li v-for="(item,index) in arr" :key="index">
+              <img :src="require('../../assets/images/my/'+item.img)"  alt="">
               <div class="div">
-                  <h4>131</h4>
-                  <span>CNY /人</span>
-                  <p>24</p>
-                  <span>234</span>
+                  <h4>{{item.title}}</h4>
+                  <span class="span1">CNY{{item.num}}/人</span>
+                  <p>{{item.txt}}</p>
+                  <span class="span2">{{item.ponp}}</span>
               </div>
           </li>
       </ul>
@@ -16,7 +16,19 @@
 
 <script>
 export default {
-
+data(){
+    return{
+        arr:[],
+    }
+},
+mounted(){
+        this.$axios.get("../../static/data/myjourney.json").then((res) => {
+      console.log(res);
+      this.arr = res.data.mydata;
+      console.log(this.arr);
+  }, 
+  )
+}
 }
 </script>
 
@@ -24,19 +36,43 @@ export default {
 @import '../../components/common/import.scss';
 .box{
     li{
-        height:toREM(150) ;
+        height:toREM(120) ;
         width: 100%;
-        background-color: aqua;
+        margin-bottom:toREM(10) ;
+        // background-color: aqua;
         img{
-            height:toREM(150);
-            width:toREM(150);
-            // width: 40%;
+            height:toREM(120);
+            width:toREM(120);
             float: left;
         }
     .div{
-         height:toREM(150);
-        background-color:red ;
-        // padding-left: ;
+         height:toREM(120);
+        background-color:#EEEEEE ;
+        // padding-left:toREM(20) 
+        float: left;
+        width: toREM(225);
+        padding-left:toREM(10) ;
+       h4{
+           line-height: toREM(20);
+           margin-top:toREM(15) ;
+       }
+       p{
+            font-size:toREM(10) ;
+            line-height:toREM(20) ;
+             margin-bottom:toREM(5) ;
+       }
+       .span1{
+            font-size:toREM(10) ;
+            line-height:toREM(30) ;
+       }
+       .span2{
+           padding: 3px 5px;
+           background-color: #C2DAD4;
+           border-radius: 6px;
+           color: #4FA48F;
+           font-size:toREM(10) ;
+            margin-top:toREM(20) ;
+       }
     }
     }
 }
